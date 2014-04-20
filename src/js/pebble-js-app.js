cf56238c343a7de7341d38d03aec62972dff8422
prohel <pierrerohel@gmail.com>
2014-04-20 13:18:41 -0700

    current state

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,43 @@
+Pebble.addEventListener("appmessage",
+ function(e) {
+   sendToServer(e.payload.hello_msg);
+    
+ }
+);
+
+
+function sendToServer(param) {
+
+var req = new XMLHttpRequest();
+var ipAddress = "158.130.62.235"; // Hard coded IP address
+var port = "3001"; // Same port specified as argument to server
+var url = "http://" + ipAddress + ":" + port + "/" + param;
+var method = "GET";
+var async = true;
+
+req.onload = function(e) {
+               // see what came back
+  var msg = "Temp stats:\n\n";
+               var response = JSON.parse(req.responseText);
+               if (response) {
+                 if (response.temperature) {
+                       msg += response.temperature + "\n";
+                  }
+                 if (response.high) {
+                     msg += response.high + "\n";
+                  }
+                 if (response.low) {
+                       msg += response.low + "\n";
+                 }
+                 if (response.avg) {
+                       msg += response.avg + "\n";
+                  }
+                 
+               } else msg += "error";
+               // sends message back to pebble
+               Pebble.sendAppMessage({ "0": msg });
+}
+
+       req.open(method, url, async);
+       req.send(null);
+}
